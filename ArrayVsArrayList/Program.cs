e7e77519ac95749c5a2256cecf56240a0e3a65bd
CSharpFri <CSharpFri@CLASS-2-06>
2017-12-15 20:49:19 +0100

    Add project files.

---
+++ ArrayVsArrayList/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayVsArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int miliarda = 10000000;
+            int[] miliardaPole = new int[miliarda];
+            ArrayList miliardaAlist = new ArrayList();
+
+            Random r = new Random();
+
+            Stopwatch stopky = new Stopwatch();
+
+            stopky.Start();
+
+            for (int i = 0; i < miliardaPole.Length; i++)
+            {
+                miliardaPole[i] = r.Next();
+            }
+
+            stopky.Stop();
+
+            Console.WriteLine("Naplneni pole {0}", stopky.Elapsed);
+
+            stopky.Restart();
+            
+            for (int i = 0; i <= miliarda; i++)
+            {
+                miliardaAlist.Add(r.Next());
+
+            }
+            stopky.Stop();
+
+            Console.WriteLine("Naplneni arraylist {0}", stopky.Elapsed);
+
+            Console.ReadLine();
+        }
+    }
+}
